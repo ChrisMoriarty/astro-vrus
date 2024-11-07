2a0ffcd249a77b37a6c2a82d7af0151471cfd3c1
Chris Moriarty <cmoriarty@gmail.com>
2024-11-07 18:09:41 -0500

    Added github action to deploy site to pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site: "https://ChrisMoriarty.github.io",
+  // base: "/moriartyengineering",
+  integrations: [tailwind(), react()],
 });
