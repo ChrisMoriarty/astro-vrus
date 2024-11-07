a7093eb7671abb421bb408d8cc564b39a7e338b0
Chris Moriarty <cmoriarty@gmail.com>
2024-11-07 18:43:06 -0500

    does this work yet??

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  public: "./public/",
   site: "https://ChrisMoriarty.github.io/",
   base: "/moriartyengineering",
   integrations: [tailwind(), react()],
